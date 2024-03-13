0b21dccca8c271972dce19ff1140db12619acf87
19maxi71 <19maxi71@gmail.com>
2024-03-13 21:22:38 +0100

    fin partie provisoire 'player' MVC

--- all_controllers/player_controller.py
+++ all_controllers/player_controller.py
@@ -1,5 +1,8 @@
-from player.py import Player
-from player_view.py import PlayerView
+import sys
+sys.path.append(r"D:\All OpenClassRooms projects\p4_checkmate_tournament\p4_checkmate_tournament")
+import json
+from all_models.player import Player
+from all_views.player_view import PlayerView
 
 
 class PlayerController:
@@ -8,6 +11,33 @@ class PlayerController:
 
     def create_player(self, name, last_name, date_of_birth, chess_id):
         player = Player(name, last_name, date_of_birth, chess_id)
+        return player
+        
+    def save_player(self, player):
+        player_data = {
+            "name": player.name,
+            "last_name": player.last_name,
+            "date_of_birth": player.date_of_birth,
+            "chess_id": player.chess_id
+        }
+        
+        
+        try:
+            with open(r"D:\All OpenClassRooms projects\p4_checkmate_tournament\p4_checkmate_tournament\all_data\players.json", 'r') as file:
+                data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = []
+
+        data.append(player_data)
+
+        with open(r"D:\All OpenClassRooms projects\p4_checkmate_tournament\p4_checkmate_tournament\all_data\players.json", "w") as file:
+            json.dump(data, file)    
         
     def show_player(self, player):
         self.player_view.display_player(player)
+        
+        
+# # juste pour test du code        
+# runfortest = PlayerController()
+# player = runfortest.create_player("John", "Doe", "1990-01-01", "123456")
+# runfortest.save_player(player)
